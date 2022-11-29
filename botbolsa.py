60213408c922b9ba2b6ae56f9e1fe7bd6396c823
Lucas de Sá Oliveira <88409787+LOliveira2305@users.noreply.github.com>
2022-11-29 12:18:37 -0300

    Atualizando com arquivo comentado

--- botbolsa.py
+++ botbolsa.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from datetime import datetime
 
+#acessando o navegador e iniciando as variáveis de dados de valorização
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 navegador = webdriver.Chrome(options=options)
@@ -19,6 +20,7 @@ porcentagens_val = []
 dados_val = []
 dict1 = {}
 
+#armazenando cada informação das empresas nas listas correspondentes, aplicando um loop nos índices dos XPaths
 for i in range(1, 7):
     empresas_val.append(navegador.find_element(By.XPATH,
                                                 f'//*[@id="today-acao"]/div/div[2]/div[1]/div/div[{i}]/div[2]/a/h4/small').text)
@@ -27,6 +29,7 @@ for i in range(1, 7):
     cotacao_val.append(navegador.find_element(By.XPATH,
                                                 f'//*[@id="today-acao"]/div/div[2]/div[1]/div/div[{i}]/div[2]/div/span[2]').text.replace("R$\n", ""))
 
+#criando um dicionário com os dados encontrados de cada empresa e armazenando-os numa lista
 for a, b, c in zip(empresas_val, cotacao_val, porcentagens_val):
     dict1 = {}
     dict1["Empresa"] = a
@@ -35,6 +38,7 @@ for a, b, c in zip(empresas_val, cotacao_val, porcentagens_val):
     dict1["Horário"] = data_atual
     dados_val.append(dict1)
 
+#armazenando cada nova série de dados em um arquivo json para posterior estudo:
 with open("valbolsa.json", "r+", encoding='utf-8') as file:
     file_data = json.load(file)
     for dados in dados_val:
@@ -42,11 +46,13 @@ with open("valbolsa.json", "r+", encoding='utf-8') as file:
        file.seek(0)
     json.dump(file_data, file, ensure_ascii=False)
 
+#apresentando uma tabela com os dados encontrados
 tabela1 = pd.DataFrame.from_records(dados_val, index = [i for i in range(1, 7)])
 print(f"A pontuação do Ibovespa no horário {data_atual} é de {float(pontuacao.replace('pts', ''))} pontos.")
 print(f"As ações mais valorizadas do Ibovespa até o momento ({data_atual} horas): ")
 print(tabela1)
 
+#faremos todas essas mesmas etapas para as desvalorizações
 empresas_desv = []
 cotacao_desv = []
 porcentagens_desv = []
